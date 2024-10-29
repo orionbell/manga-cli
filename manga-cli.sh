2e1812999aa49da350e8cb276001c02ab939bea3
WinterEffect <yishaigame999@gmail.com>
2024-10-29 19:47:10 +0200

    finish page url extraction (get_pages and get_page functions)

--- manga-cli.sh
+++ manga-cli.sh
@@ -2,9 +2,6 @@
 
 setup(){
     clear
-    if [ ! -d "$DIRECTORY" ]; then
-        mkdir -p ~/.cache/manga-cli
-    fi
 }
 get_fullname(){
     search_results=$(curl --silent -G "https://api.mangadex.org/manga" --data-urlencode "title=$name" | jq -r ".data[]")
@@ -28,7 +25,22 @@ get_chapter(){
     chapter=$(echo $chapters | jq -r '"Vol \(.attributes.volume) Chapter \(.attributes.chapter)"'| fzf --height 30% --border)
     echo "$chapter"
 }
-#download_page(){}
+get_pages(){
+    selected_chapters=$(curl --silent "https://api.mangadex.org/manga/$1/feed" | jq -r ".data[] | select(.attributes.volume==\"$2\" and .attributes.chapter==\"$3\")" )
+    lang=$(echo $selected_chapters | jq -r ".attributes.translatedLanguage" | fzf --height 20% --border)
+    chapter_id=$(echo $selected_chapters | jq -r ". | select(.attributes.translatedLanguage==\"$lang\").id")
+    pages_obj=$(curl --silent "https://api.mangadex.org/at-home/server/$chapter_id")
+    if [ -z "$pages_obj" ]; then
+        exit
+    fi
+    echo $pages_obj
+}
+get_page(){
+    base_url=$(echo "$1" | jq -r '.baseUrl')
+    chapter_hash=$(echo "$1" | jq -r '.chapter.hash')
+    page=$(echo "$1" | jq -r ".chapter.data[$2]")
+    echo "$base_url/data/$chapter_hash/$page"
+}
 #clean(){}
 
 setup
@@ -50,7 +62,9 @@ fi
 volume=$(echo "$chapter" | awk '{print $2}')
 chapter=$(echo "$chapter" | awk '{print $4}')
 
-echo "$volume"
-echo "$chapter"
-
-
+pages_obj=$(get_pages $id "$volume" "$chapter")
+length=$(echo $pages_obj | jq -r '.chapter.data | length')
+num=0
+page=$(get_page "$pages_obj" $num)
+clear
+kitty icat "$page"
